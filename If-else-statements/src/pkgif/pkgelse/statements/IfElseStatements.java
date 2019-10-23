eb17d7963edd3d25676edda76f91b473aafa9f0b
20176 <20176@MININT-VTTS8LK.columbushs.com>
2019-10-23 14:30:54 -0400

    ok

--- If-else-statements/src/pkgif/pkgelse/statements/IfElseStatements.java
+++ If-else-statements/src/pkgif/pkgelse/statements/IfElseStatements.java
@@ -1,73 +1,98 @@
-
 package pkgif.pkgelse.statements;
 
 // Import Scanner Here
-
-
+import java.util.Scanner;
 
 /**
  *
- * Name:
- * Period:
- * Project Name:
- * 
- * 
+ * Name: Period: Project Name:
+ *
+ *
  */
 public class IfElseStatements {
 
     // Create Scanner Object here So it may be used in all methods
-    
-    
-    
+    private static Scanner kb = new Scanner(System.in);
+
     public static void main(String[] args) {
         // Happy Coding!
-        
-        
+
+        question1();
         //Invoke the methods here 
-        
-        
-        
+
     }
-    
-    
-    public static void question1(){
-        
+
+    public static void question1() {
+        System.out.println("Enter your grade to see if you passed or failed the exam!");
+        int num = kb.nextInt();
+
+        if (num >= 70) {
+            System.out.println("You have PASSED");
+        } else {
+            System.out.println("You have... Failed :-{");
+        }
     }
-    
-    public static void question2(){
-        
+
+    public static void question2() {
+        System.out.println("Enter a number");
+        int num = kb.nextInt();
+
+        if (num <= 50) {
+            System.out.println("GO");
+        } else {
+            System.out.println("STOP");
+        }
     }
-    
-    public static void question3(){
-        
+
+    public static void question3() {
+        System.out.println("Enter a number");
+        int num = kb.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
     }
-    
-    public static void question4(){
-        
+
+    public static void question4() {
+        System.out.println("Enter a number");
+        int num = kb.nextInt();
     }
+        if (num % 5 == 0) {
+            System.out.println("MULTIPLE OF 5");
+        }
     
-    public static void question5(){
+
+   
+        else
+        {
         
+System.out.println("NOT A MULTIPLE OF 5");
     }
-    
-    public static void question6(){
-        
+
+    public static void question5() {
+
     }
-    
-    public static void question7(){
-        
+
+    public static void question6() {
+
     }
-    
-    public static void question8(){
-        
+
+    public static void question7() {
+
     }
-    
-    public static void question9(){
-        
+
+    public static void question8() {
+
     }
-    
-    public static void question10(){
-        
+
+    public static void question9() {
+
     }
-    
+
+    public static void question10() {
+
+    }
+
 }
